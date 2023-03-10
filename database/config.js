01a050362fbd74cc9f95f825e9641e154f3f3ff2
Fernando Requena <fernandorequenaserra@gmail.com>
2023-03-10 15:17:33 +0100

    vercel2

--- database/config.js
+++ database/config.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const { MONGODB_CNN } = process.env;
-const connectionEnv = String(MONGODB_CNN);
-
 const dbConnection = async () => {
   try {
-    await mongoose.connect(connectionEnv);
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Base de datos online");
   } catch (error) {
     throw new Error(error);
