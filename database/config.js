9b0b49afd31929df609b73344113aa948c606e85
Fernando Requena <fernandorequenaserra@gmail.com>
2023-03-12 14:59:45 +0100

    variando config de env

--- database/config.js
+++ database/config.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CNN, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_CNN);
     console.log("Base de datos online");
   } catch (error) {
     throw new Error(error);
